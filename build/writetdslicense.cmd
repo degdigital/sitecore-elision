b32646ad822a8579a7a6093286c6ab5492954c19
Patrick Delancy <PDelancy@degdigital.com>
2016-02-27 00:14:41 -0600

    updated sitecore package references to that deploy dlls are available for tds package

--- build/writetdslicense.cmd
+++
@@ -1,4 +0,0 @@
-PUSHD "%~dp0..\"
-echo ^<?xml version="1.0" encoding="utf-8" ?^>>build\HedgehogDevelopment\SitecoreProject\v9.0\TDSLicense.config
-echo ^<license Owner="%1" Key="%2" /^>>>build\HedgehogDevelopment\SitecoreProject\v9.0\TDSLicense.config
-POPD
