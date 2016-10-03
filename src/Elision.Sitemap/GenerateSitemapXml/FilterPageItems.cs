e05d6b184739eae72bd0abc7cad22ce152543c1f
Jeffrey Rondeau <jrondeau@degdigital.com>
2016-10-03 12:10:34 -0500

    Add filter page logic into sitemap module

---
+++ src/Elision.Sitemap/GenerateSitemapXml/FilterPageItems.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Elision.Fields;
+
+namespace Elision.Sitemap.GenerateSitemapXml
+{
+    public class FilterPageItems : IGenerateSitemapProcessor
+    {
+        public void Process(GenerateSitemapArgs args)
+        {
+            if (args.Items.Any())
+                args.Items = args.Items.Where(x => x.InheritsFrom(ContentPageFieldIDs.TemplateId));
+        }
+    }
+}
